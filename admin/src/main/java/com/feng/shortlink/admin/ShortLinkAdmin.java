4abf43fd282b71ba689d9156b5c2bae59c6630d5
枫 <fx20031215@163.com>
2024-09-25 01:28:20 +0800

    feat: 枫短链模块搭建

---
+++ admin/src/main/java/com/feng/shortlink/admin/ShortLinkAdmin.java
@@ -0,0 +1,18 @@
+package com.feng.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author FENGXIN
+ * @date 2024/9/24
+ * @project feng-shortlink
+ * @description
+ **/
+@SpringBootApplication
+public class ShortLinkAdmin {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdmin.class, args);
+    }
+    
+}
