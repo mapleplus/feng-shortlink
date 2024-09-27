eac12c4f5df422bc46ce56199912d08ba4a0b440
枫 <fx20031215@163.com>
2024-09-27 22:31:04 +0800

    feat:    添加数据加密存储规则到shardingsphere-config.yaml style:   改善ShortLinkAdminTest.java中SQL字符串格式

--- admin/src/test/java/com/feng/shortlink/admin/test/ShortLinkAdminTest.java
+++ admin/src/test/java/com/feng/shortlink/admin/test/ShortLinkAdminTest.java
@@ -10,20 +10,21 @@ import org.springframework.boot.test.context.SpringBootTest;
  **/
 @SpringBootTest
 public class ShortLinkAdminTest {
-    private static String sql = "create table t_user%d\n" +
-            "(\n" +
-            "    id            bigint auto_increment comment 'ID'\n" +
-            "        primary key,\n" +
-            "    username      varchar(256) null comment '用户名',\n" +
-            "    password      varchar(512) null comment '密码',\n" +
-            "    real_name     varchar(256) null comment '真实姓名',\n" +
-            "    phone         varchar(128) null comment '手机号',\n" +
-            "    mail          varchar(512) null comment '邮箱',\n" +
-            "    deletion_time bigint       null comment '注销时间戳',\n" +
-            "    create_time   datetime     null comment '创建时间',\n" +
-            "    update_time   datetime     null comment '修改时间',\n" +
-            "    del_flag      tinyint(1)   null comment '删除标识 0：未删除 1：已删除'\n" +
-            ");";
+    private static final String sql = """
+            create table t_user%d
+            (
+                id            bigint auto_increment comment 'ID'
+                    primary key,
+                username      varchar(256) null comment '用户名',
+                password      varchar(512) null comment '密码',
+                real_name     varchar(256) null comment '真实姓名',
+                phone         varchar(128) null comment '手机号',
+                mail          varchar(512) null comment '邮箱',
+                deletion_time bigint       null comment '注销时间戳',
+                create_time   datetime     null comment '创建时间',
+                update_time   datetime     null comment '修改时间',
+                del_flag      tinyint(1)   null comment '删除标识 0：未删除 1：已删除'
+            );""";
     public static void main (String[] args) {
         for (int i = 0; i < 16; i++) {
             System.out.printf ((sql) + "%n" ,i);
