29a73917153be0a965b02d0b6620e7e217aa17f3
枫 <fx20031215@163.com>
2024-09-30 00:01:20 +0800

    feat(sharding):    添加ShardingSphere配置以支持分库分表 chore(database):    将数据库连接配置更改为使用ShardingSphere test(database):    更新测试用例以创建t_link表而非t_user表

--- admin/src/test/java/com/feng/shortlink/admin/test/ShortLinkAdminTest.java
+++ admin/src/test/java/com/feng/shortlink/admin/test/ShortLinkAdminTest.java
@@ -11,20 +11,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class ShortLinkAdminTest {
     private static final String sql = """
-            create table t_user%d
-            (
-                id            bigint auto_increment comment 'ID'
-                    primary key,
-                username      varchar(256) null comment '用户名',
-                password      varchar(512) null comment '密码',
-                real_name     varchar(256) null comment '真实姓名',
-                phone         varchar(128) null comment '手机号',
-                mail          varchar(512) null comment '邮箱',
-                deletion_time bigint       null comment '注销时间戳',
-                create_time   datetime     null comment '创建时间',
-                update_time   datetime     null comment '修改时间',
-                del_flag      tinyint(1)   null comment '删除标识 0：未删除 1：已删除'
-            );""";
+            create table t_link%d
+         (
+             id              bigint auto_increment comment 'ID'
+                 primary key,
+             domain          varchar(128)  null comment '域名',
+             short_uri       varchar(8)    null comment '短链接',
+             full_short_url  varchar(128)  null comment '完整短链接',
+             origin_url      varchar(1024) null comment '原始链接',
+             click_num       int default 0 null comment '点击量',
+             gid             varchar(32)   null comment '分组标识',
+             enable_status   tinyint(1)    null comment '启用标识 0：已启用 1：未启用',
+             created_type    tinyint(1)    null comment '创建类型 0：接口 1：控制台',
+             valid_date_type tinyint(1)    null comment '有效期类型 0：永久有效 1：用户自定义',
+             valid_date      datetime      null comment '有效期',
+             `describe`      varchar(1024) null comment '描述',
+             create_time     datetime      null comment '创建时间',
+             update_time     datetime      null comment '修改时间',
+             del_flag        tinyint(1)    null comment '删除标识 0：未删除 1：已删除',
+             constraint idx_unique_full_short_url
+                 unique (full_short_url)
+         );""";
     public static void main (String[] args) {
         for (int i = 0; i < 16; i++) {
             System.out.printf ((sql) + "%n" ,i);
