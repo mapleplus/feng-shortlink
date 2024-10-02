b040b935556a53c35750e261bc24f1c2de1af34c
枫 <fx20031215@163.com>
2024-10-02 16:23:22 +0800

    feat: 添加路由表实体和mapper

---
+++ project/src/main/java/com/feng/shortlink/project/dao/entity/LinkGotoDO.java
@@ -0,0 +1,35 @@
+package com.feng.shortlink.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author FENGXIN
+ * @date 2024/10/2
+ * @project feng-shortlink
+ * @description
+ **/
+@Data
+@TableName("t_link_goto")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LinkGotoDO {
+    /**
+     * ID
+     */
+    private Long id;
+    
+    /**
+     * 分组标识
+     */
+    private String gid;
+    
+    /**
+     * 完整短 URL
+     */
+    private String fullShortUrl;
+}
---
+++ project/src/main/java/com/feng/shortlink/project/dao/mapper/LinkGotoMapper.java
@@ -0,0 +1,13 @@
+package com.feng.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.feng.shortlink.project.dao.entity.LinkGotoDO;
+
+/**
+ * @author FENGXIN
+ * @date 2024/9/29
+ * @project feng-shortlink
+ * @description 短链接路由mapper
+ **/
+public interface LinkGotoMapper extends BaseMapper<LinkGotoDO> {
+}
