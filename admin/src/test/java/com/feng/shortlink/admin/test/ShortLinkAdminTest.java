f52c044ee90a15daa517db47dce630df3f007617
枫 <fx20031215@163.com>
2024-10-02 16:23:50 +0800

    feat: 添加路由表分片（full_short_link）

--- admin/src/test/java/com/feng/shortlink/admin/test/ShortLinkAdminTest.java
+++ admin/src/test/java/com/feng/shortlink/admin/test/ShortLinkAdminTest.java
@@ -11,20 +11,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class ShortLinkAdminTest {
     private static final String sql = """
-            create table t_user_%d
-            (
-                id            bigint auto_increment comment 'ID'
-                    primary key,
-                username      varchar(256) null comment '用户名',
-                password      varchar(512) null comment '密码',
-                real_name     varchar(256) null comment '真实姓名',
-                phone         varchar(128) null comment '手机号',
-                mail          varchar(512) null comment '邮箱',
-                deletion_time bigint       null comment '注销时间戳',
-                create_time   datetime     null comment '创建时间',
-                update_time   datetime     null comment '修改时间',
-                del_flag      tinyint(1)   null comment '删除标识 0：未删除 1：已删除'
-            );""";
+            create table t_link_goto_%d
+              (
+                  id             bigint auto_increment comment 'ID'
+                      primary key,
+                  gid            varchar(32)  null comment 'GID',
+                  full_short_url varchar(128) null comment '完整短链接'
+              )
+                  comment '路由表';""";
     public static void main (String[] args) {
         for (int i = 0; i < 16; i++) {
             System.out.printf ((sql) + "%n" ,i);
